681ae9fb7b9118bc30980e133bcf9904b673f805
Flore Tallerand <makelita@Flores-MacBook-Pro.local>
2022-12-24 17:01:00 -0500

    Title Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,6 +33,7 @@ const Home = ({ textInfo, setTextInfo }) => {
 
   return (
     <div className="home">
+        <h1>Digraph</h1>
       <form onSubmit={handleSubmit}>
         <textarea
           placeholder="past text here."
